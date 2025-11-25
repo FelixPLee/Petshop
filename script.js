ac877d37aeb5f25f10470442de596939a3135b16
Felix <felixpinheirolee@gmail.com>
2025-11-25 14:22:18 -0300

    adding bootstrap

---
+++ script.js
@@ -0,0 +1,19 @@
+`use strict`
+
+
+//
+const User = class {
+    constructor({...userInfo}){
+        this.name = name
+        this.address = address
+        this.cpf = cpf
+        this.gender = gender
+        this.telephone = telephone
+        this.email = email
+        this.password = password
+    }
+}
+
+const getUserinfo = function() {
+    
+}
