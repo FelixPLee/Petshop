107ec987f0583d35fa8cc160688f2098af6647fc
Felix <felixpinheirolee@gmail.com>
2025-11-27 08:40:04 -0300

    color testing

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ const User = class {
 const getUserinfo = function() {
     
 }
+
+// Seleciona o elemento DOM
+const myCarouselElement = document.querySelector('#meuCarrossel');
+
+// Inicializa com opções customizadas
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 3000, // Tempo em milissegundos entre transições (Padrão: 5000)
+  wrap: true,     // Se o carrossel deve voltar ao início após o último slide
+  touch: true     // Habilita gestos de "swipe" em dispositivos touch
+});
